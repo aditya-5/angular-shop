fa9aa691a687060be545dd13dad64fb9c54a3266
u07944aa <aditya@student.manchester.ac.uk>
2021-02-21 18:24:44 +0530

    Fixed a resolver bug (overwriting changes)

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -3,17 +3,22 @@ import {Observable } from "rxjs";
 import {Injectable} from "@angular/core";
 import {Recipe } from "./recipe.model";
 import {DataStorageService} from "../shared/data-storage.service";
-
+import {RecipeService} from "./recipe.service";
 @Injectable({
   providedIn:'root'
 })
 
 export class RecipesResolverService implements Resolve<Recipe[]> {
-  constructor(private dataStorageService :  DataStorageService){
+  constructor(private dataStorageService :  DataStorageService, private recipeService:RecipeService){
   }
 
   resolve (route : ActivatedRouteSnapshot, state: RouterStateSnapshot){
     console.log("started resolving");
-    return this.dataStorageService.fetchRecipes();
+    const newRec = this.dataStorageService.fetchRecipes();
+    const rec = this.recipeService.getRecipes();
+    if(rec.length === 0)
+    return newRec;
+    else
+    return rec;
   }
 }
